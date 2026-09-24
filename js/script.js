ae8f3e3dd17a80d9074c55876f907442491f2ebc
momokaaa-prog <izumichi0117@outlook.jp>
2026-09-24 22:06:41 +0900

    logo2

--- js/script.js
+++ js/script.js
@@ -10,16 +10,6 @@ $(function () {
     });
 });
 
-// $(function () {
-//     $(window).on("scroll", function () {
-//         const sliderHeight = $(".header").height();
-//         if (sliderHeight - 30 < $(this).scrollTop()) {
-//         $(".header").addClass("headerColorScroll");
-//         } else {
-//         $(".header").removeClass("headerColorScroll");
-//         }
-//     });
-// });
 
 $(function () {
     $(window).on("scroll", function () {
